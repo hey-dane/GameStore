f16725625575bce29279805aba8cfdeda9260a05
Dana <126463047+hey-dane@users.noreply.github.com>
2023-08-16 20:43:39 -0700

    tasks complete

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 8080;
 
 // init morgan
-const morgan = require('morgan');
-app.use(morgan('dev'));
+const morgan = require("morgan");
+app.use(morgan("dev"));
 
 // init body-parser
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 // init cors
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 // init db client
-const client = require('./db/client');
+const client = require("./db/client");
 client.connect();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("What are we doing?");
 });
 
 // Router: /api
-app.use('/api', require('./api'));
+app.use("/api", require("./api"));
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
